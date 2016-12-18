a9ded1f800132b2d20cbf05d03e8426f84d97019
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2016-12-18 15:29:59 +0200

    + Initial push notifications setup

--- app/src/main/java/com/codeground/adventurousbulgaria/MainApplication.java
+++ app/src/main/java/com/codeground/adventurousbulgaria/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.appevents.AppEventsLogger;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
 import com.parse.SaveCallback;
@@ -25,11 +26,17 @@ public class MainApplication extends Application {
                         .applicationId("Aev0cw9ckqsWq9BGiGfnPXACPbLTHypE0ZpejrPQ")
                         .clientKey("6pLQaBu3BcEceHbTXGkOyWZs4J4qVe8EVwEUlfkN")
                         .server("https://parseapi.back4app.com/").build());
+        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
 
         //Facebook SDK Initialization
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         ParseFacebookUtils.initialize(this);
+
+        // Update Installation
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("GCMSenderId", "838294079504");
+        installation.saveInBackground();
 
         ParsePush.subscribeInBackground("visit_channel", new SaveCallback() {
             @Override
