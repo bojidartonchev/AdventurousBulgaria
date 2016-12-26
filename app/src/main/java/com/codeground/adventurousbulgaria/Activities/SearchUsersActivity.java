28c7ca1ce9924dc49215760abaecce0c02727c95
bojidar.tonchev <bojidar.tonchev@gmail.com>
2016-12-26 12:30:50 +0200

    * Unfollowing prompt added. + Cloud code for unfollowing user

--- app/src/main/java/com/codeground/adventurousbulgaria/Activities/SearchUsersActivity.java
+++ app/src/main/java/com/codeground/adventurousbulgaria/Activities/SearchUsersActivity.java
@@ -1,16 +1,18 @@
 package com.codeground.adventurousbulgaria.Activities;
 
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.codeground.adventurousbulgaria.Enums.UnfollowActions;
 import com.codeground.adventurousbulgaria.Interfaces.IOnParseItemClicked;
 import com.codeground.adventurousbulgaria.R;
 import com.codeground.adventurousbulgaria.Utilities.SearchedResultsAdapter;
@@ -22,6 +24,9 @@ import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 
 import java.util.HashMap;
+
+import static com.codeground.adventurousbulgaria.Enums.UnfollowActions.REMOVE_PENDING_FROM_USER;
+import static com.codeground.adventurousbulgaria.Enums.UnfollowActions.UNFOLLOW_USER;
 
 
 public class SearchUsersActivity extends AppCompatActivity implements View.OnClickListener, IOnParseItemClicked {
@@ -71,9 +76,9 @@ public class SearchUsersActivity extends AppCompatActivity implements View.OnCli
             if(followBtn.getText().toString().equals(getString(R.string.follow_btn_text))){
                 followUser(user, followBtn);
             }else if(followBtn.getText().toString().equals(getString(R.string.followed_btn_text))){
-                promptUnfollow(user, followBtn);
+                promptUnfollow(user, followBtn, UNFOLLOW_USER);
             }else if(followBtn.getText().toString().equals(getString(R.string.pending_follow_btn_text))){
-                promptUnfollow(user, followBtn);
+                promptUnfollow(user, followBtn, REMOVE_PENDING_FROM_USER);
             }
 
         }
@@ -102,28 +107,49 @@ public class SearchUsersActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
-    private void unfollowUser(ParseObject user, final Button followBtn) {
-       //TODO Implement
+    private void unfollowUser(ParseObject user, final Button followBtn, UnfollowActions action) {
+        boolean isFollowed = (action == UNFOLLOW_USER);
+        if(ParseUser.getCurrentUser() != null && user != null){
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("targetUserId", user.getObjectId());
+            params.put("isFollowed", isFollowed);
+            ParseCloud.callFunctionInBackground("unfollowUser", params, new FunctionCallback<Integer>() {
+                public void done(Integer result, ParseException e) {
+                    if (e == null){
+                        if(followBtn != null){
+                            followBtn.setText(R.string.follow_btn_text);
+                        }
+                    }else{
+                        //error
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 
-    private void promptUnfollow(ParseObject user, final Button followBtn){
-        //TODO Use custom dialog here for better look
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        // Add the buttons
-        builder.setPositiveButton(R.string.follow_positive_button, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // User clicked OK button
+    private void promptUnfollow(final ParseObject user, final Button followBtn, final UnfollowActions action){
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View unfollowDialogView = factory.inflate(R.layout.custom_dialog, null);
+        final AlertDialog unfollowDialog = new AlertDialog.Builder(this).create();
+        unfollowDialog.setView(unfollowDialogView);
+        TextView textField = (TextView)unfollowDialogView.findViewById(R.id.text_dialog);
+        textField.setText(String.format(getString(R.string.unfollow_prompt_text), user.getString("first_name")));
+        unfollowDialogView.findViewById(R.id.btn_dialog_yes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //your business logic
+                unfollowUser(user, followBtn, action);
+                unfollowDialog.dismiss();
             }
         });
-        builder.setNegativeButton(R.string.follow_negative_button, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // User cancelled the dialog
+        unfollowDialogView.findViewById(R.id.btn_dialog_no).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unfollowDialog.dismiss();
             }
         });
-        // Set other dialog properties
 
-        // Create the AlertDialog
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        unfollowDialog.show();
     }
 }
