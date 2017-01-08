78c02fc9fb63852efa1aa4f2a19bf209e51b4566
Bozhidar Tonchev <bojidar.tonchev@gmail.com>
2017-01-08 02:42:23 +0200

    * Fixed bug: Client does not update on launch (parse settings)

--- app/src/main/java/com/codeground/adventurousbulgaria/Utilities/SettingsManager.java
+++ app/src/main/java/com/codeground/adventurousbulgaria/Utilities/SettingsManager.java
@@ -4,18 +4,27 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 public class SettingsManager {
-    public static void updatePrefsProfileSettings(Context ctx) {
-        boolean isPrivate = false;
-        if(ParseUser.getCurrentUser().has("is_follow_allowed")){
-            isPrivate = !ParseUser.getCurrentUser().getBoolean("is_follow_allowed");
-        }
-        SharedPreferences.Editor prefs = PreferenceManager.getDefaultSharedPreferences(ctx).edit();
-        prefs.putBoolean("toggle_private_profile", isPrivate);
-        prefs.commit();
+    public static void updatePrefsProfileSettings(final Context ctx) {
+        ParseUser.getCurrentUser().fetchInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject user, ParseException e) {
+                boolean isPrivate = false;
+                if(user.has("is_follow_allowed")){
+                    isPrivate = !user.getBoolean("is_follow_allowed");
+                }
+                SharedPreferences.Editor prefs = PreferenceManager.getDefaultSharedPreferences(ctx).edit();
+                prefs.putBoolean("toggle_private_profile", isPrivate);
+                prefs.commit();
+            }
+        });
+
     }
 
     public static void updateDeviceInstallationInfo(){
