368981922d4c4f8fddefc6493454becf43ba3689
bojidar.tonchev <bojidar.tonchev@gmail.com>
2017-04-14 18:58:00 +0300

    - Search users / Pending followers activities redesign

--- app/src/main/java/com/codeground/wanderlustbulgaria/Activities/PendingFollowersActivity.java
+++ app/src/main/java/com/codeground/wanderlustbulgaria/Activities/PendingFollowersActivity.java
@@ -2,15 +2,24 @@ package com.codeground.wanderlustbulgaria.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.codeground.wanderlustbulgaria.R;
 import com.codeground.wanderlustbulgaria.Utilities.Adapters.PendingFollowersAdapter;
+import com.codeground.wanderlustbulgaria.Utilities.DialogWindowManager;
+import com.codeground.wanderlustbulgaria.Utilities.NotificationsManager;
 import com.parse.ParseQueryAdapter;
+import com.sdsmdg.tastytoast.TastyToast;
 
-public class PendingFollowersActivity extends AppCompatActivity {
+import java.util.List;
+
+public class PendingFollowersActivity extends AppCompatActivity implements ParseQueryAdapter.OnQueryLoadListener {
     private ParseQueryAdapter mAdapter;
     private ListView mResults;
+    private TextView mNoItemsLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +27,48 @@ public class PendingFollowersActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_pending_followers);
 
-        mResults=(ListView) findViewById(R.id.pending_users_list);
+        mResults = (ListView) findViewById(R.id.pending_users_list);
+        mNoItemsLabel = (TextView) findViewById(R.id.pending_followers_no_items_label);
 
         mAdapter = new PendingFollowersAdapter(this);
         mAdapter.setTextKey("title");
+        mAdapter.addOnQueryLoadListener(this);
         mResults.setAdapter(mAdapter);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.pending_wanderers_title);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLoading() {
+        DialogWindowManager.show(this);
+    }
+
+    @Override
+    public void onLoaded(List objects, Exception e) {
+
+        if(e==null){
+            if(objects!=null && objects.size() > 0){
+                mResults.setVisibility(View.VISIBLE);
+                mNoItemsLabel.setVisibility(View.GONE);
+            }else{
+                mNoItemsLabel.setVisibility(View.VISIBLE);
+            }
+        }else{
+            mNoItemsLabel.setVisibility(View.VISIBLE);
+            NotificationsManager.showToast(e.getMessage(), TastyToast.ERROR);
+        }
+        DialogWindowManager.dismiss();
     }
 }
--- app/src/main/java/com/codeground/wanderlustbulgaria/Activities/SearchUsersActivity.java
+++ app/src/main/java/com/codeground/wanderlustbulgaria/Activities/SearchUsersActivity.java
@@ -3,17 +3,21 @@ package com.codeground.wanderlustbulgaria.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.codeground.wanderlustbulgaria.R;
 import com.codeground.wanderlustbulgaria.Utilities.Adapters.SearchedResultsAdapter;
 import com.codeground.wanderlustbulgaria.Utilities.DialogWindowManager;
+import com.codeground.wanderlustbulgaria.Utilities.NotificationsManager;
 import com.parse.ParseObject;
 import com.parse.ParseQueryAdapter;
+import com.sdsmdg.tastytoast.TastyToast;
 
 import java.util.List;
 
@@ -22,6 +26,7 @@ public class SearchUsersActivity extends AppCompatActivity implements View.OnCli
     private Button mSearchBtn;
     private EditText mSearchField;
     private ListView mResults;
+    private TextView mNoResultsLabel;
 
 
     private ParseQueryAdapter mAdapter;
@@ -35,9 +40,13 @@ public class SearchUsersActivity extends AppCompatActivity implements View.OnCli
         mSearchBtn = (Button) findViewById(R.id.search_btn);
         mSearchField = (EditText) findViewById(R.id.search_query);
         mResults=(ListView) findViewById(R.id.searched_users);
+        mNoResultsLabel = (TextView) findViewById(R.id.search_no_items_label);
         mResults.setOnItemClickListener(this);
 
         mSearchBtn.setOnClickListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.search_users_title);
     }
 
     void search(){
@@ -73,6 +82,28 @@ public class SearchUsersActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onLoaded(List objects, Exception e) {
+        if(e==null){
+            if(objects!=null && objects.size() > 0){
+                mResults.setVisibility(View.VISIBLE);
+                mNoResultsLabel.setVisibility(View.GONE);
+            }else{
+                mNoResultsLabel.setVisibility(View.VISIBLE);
+            }
+        }else{
+            mNoResultsLabel.setVisibility(View.VISIBLE);
+            NotificationsManager.showToast(e.getMessage(), TastyToast.ERROR);
+        }
         DialogWindowManager.dismiss();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
