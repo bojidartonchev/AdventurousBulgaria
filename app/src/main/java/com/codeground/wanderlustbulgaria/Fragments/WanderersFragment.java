ea0b28914be4f71d7a5393612b5d1acfa3f60159
bojidar.tonchev <bojidar.tonchev@gmail.com>
2017-04-14 20:13:53 +0300

    * Bug fixed: notify user if no wanderers available in chat screen - User ParseQueryAdapter for the chat. * Bug fixed: chat users profile picture mismatch

--- app/src/main/java/com/codeground/wanderlustbulgaria/Fragments/WanderersFragment.java
+++ app/src/main/java/com/codeground/wanderlustbulgaria/Fragments/WanderersFragment.java
@@ -1,6 +1,7 @@
 package com.codeground.wanderlustbulgaria.Fragments;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,24 +10,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.codeground.wanderlustbulgaria.Activities.ChatActivity;
+import com.codeground.wanderlustbulgaria.Activities.SearchUsersActivity;
 import com.codeground.wanderlustbulgaria.R;
+import com.codeground.wanderlustbulgaria.Utilities.NotificationsManager;
 import com.codeground.wanderlustbulgaria.Utilities.RoundedParseImageView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.sdsmdg.tastytoast.TastyToast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class WanderersFragment extends Fragment{
+public class WanderersFragment extends Fragment implements ParseQueryAdapter.OnQueryLoadListener, AdapterView.OnItemClickListener, View.OnClickListener{
 
     /** The Chat list. */
     private ArrayList<ParseUser> uList;
@@ -36,6 +42,11 @@ public class WanderersFragment extends Fragment{
 
     private View mContainer;
     private View mProgress;
+    private RelativeLayout mNoItemsLabel;
+    private Button mFindFriendButton;
+
+    private ParseQueryAdapter mAdapter;
+    private ListView mResults;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,8 +54,20 @@ public class WanderersFragment extends Fragment{
 
         mContainer = v.findViewById(R.id.list_container);
         mProgress = v.findViewById(R.id.list_progress);
+        mNoItemsLabel = (RelativeLayout)v.findViewById(R.id.chat_no_items_label);
+        mFindFriendButton = (Button)mNoItemsLabel.findViewById(R.id.chat_find_user_btn);
+        mFindFriendButton.setOnClickListener(this);
 
         user = ParseUser.getCurrentUser();
+
+        mResults = (ListView) v.findViewById(R.id.list);
+        mResults.setOnItemClickListener(this);
+
+        mAdapter = new UserAdapter(getActivity());
+        mAdapter.setTextKey("title");
+        mAdapter.setImageKey("photo");
+        mAdapter.addOnQueryLoadListener(this);
+        mResults.setAdapter(mAdapter);
 
         return v;
     }
@@ -59,7 +82,7 @@ public class WanderersFragment extends Fragment{
     @Override
     public void onResume() {
         super.onResume();
-        loadUserList();
+        mAdapter.loadObjects();
     }
 
     @Override
@@ -67,99 +90,71 @@ public class WanderersFragment extends Fragment{
         super.onDestroyView();
     }
 
-    /**
-     * Load list of users.
-     */
-    private void loadUserList()
-    {
-        ParseUser.getQuery().whereNotEqualTo("username", user.getUsername()).findInBackground(new FindCallback<ParseUser>() {
-            @Override
-            public void done(List<ParseUser> users, ParseException e) {
-                if(mContainer != null && mContainer.getVisibility() == View.GONE){
-                    //show the chat list
-                    mContainer.setVisibility(View.VISIBLE);
+    @Override
+    public void onLoading() {
 
-                    if(mProgress!=null)
-                    {
-                        mProgress.setVisibility(View.GONE);
-                    }
-                }
-
-                if(users != null){
-                    if(users.size() == 0){
-                        Toast.makeText(getActivity(), "No users found", Toast.LENGTH_SHORT);
-                    }
-
-                    uList = new ArrayList<ParseUser>(users);
-
-                    if(getView()!=null){
-                        ListView list = (ListView) getView().findViewById(R.id.list);
-                        list.setAdapter(new WanderersFragment.UserAdapter());
-                        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                Intent intent = new Intent(getActivity(), ChatActivity.class);
-                                intent.putExtra("username", uList.get(position).getUsername());
-                                intent.putExtra("full_name", uList.get(position).getString("first_name") + " " + uList.get(position).getString("last_name"));
-                                startActivity(intent);
-                            }
-                        });
-                    }
-
-                }else{
-                    //TODO notify error with custom dialog
-                }
-            }
-        });
     }
 
-    /**
-     * The Class UserAdapter is the adapter class for User ListView. This
-     * adapter shows the user name and it's only online status for each item.
-     */
-    private class UserAdapter extends BaseAdapter
+    @Override
+    public void onLoaded(List objects, Exception e) {
+        mProgress.setVisibility(View.GONE);
+
+        if(e==null){
+            if(objects!=null && objects.size() > 0){
+                mContainer.setVisibility(View.VISIBLE);
+                mNoItemsLabel.setVisibility(View.GONE);
+            }else{
+                mNoItemsLabel.setVisibility(View.VISIBLE);
+            }
+        }else{
+            mNoItemsLabel.setVisibility(View.VISIBLE);
+            NotificationsManager.showToast(e.getMessage(), TastyToast.ERROR);
+        }
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ParseUser entry = (ParseUser) parent.getItemAtPosition(position);
+        Intent intent = new Intent(getActivity(), ChatActivity.class);
+        intent.putExtra("username", entry.getUsername());
+        intent.putExtra("full_name", entry.getString("first_name") + " " + entry.getString("last_name"));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.chat_find_user_btn){
+            Intent intent = new Intent(getContext(), SearchUsersActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private class UserAdapter extends ParseQueryAdapter
     {
 
-        /* (non-Javadoc)
-         * @see android.widget.Adapter#getCount()
-         */
-        @Override
-        public int getCount()
+        public UserAdapter(Context context)
         {
-            return uList.size();
+            super(context, new ParseQueryAdapter.QueryFactory<ParseUser>() {
+                public ParseQuery create() {
+                    ParseUser user = ParseUser.getCurrentUser();
+                    ParseRelation<ParseUser> followingRelation = user.getRelation("following");
+                    ParseQuery<ParseUser> q = followingRelation.getQuery();
+
+                    return q;
+                }
+            });
         }
 
-        /* (non-Javadoc)
-         * @see android.widget.Adapter#getItem(int)
-         */
         @Override
-        public ParseUser getItem(int arg0)
-        {
-            return uList.get(arg0);
-        }
-
-        /* (non-Javadoc)
-         * @see android.widget.Adapter#getItemId(int)
-         */
-        @Override
-        public long getItemId(int arg0)
-        {
-            return arg0;
-        }
-
-        /* (non-Javadoc)
-
-         * @see android.widget.Adapter#getView(int, android.view.View, android.view.ViewGroup)
-         */
-        @Override
-        public View getView(int pos, View v, ViewGroup arg2)
-        {
+        public View getItemView(final ParseObject object, View v, ViewGroup parent) {
             if (v == null)
                 v = getActivity().getLayoutInflater().inflate(R.layout.chat_item, null);
 
-            ParseUser c = getItem(pos);
-            RoundedParseImageView imgView = (RoundedParseImageView) v.findViewById(R.id.profile_image);
-            ParseFile imageFile = c.getParseFile("profile_picture");
+            super.getItemView(object, v, parent);
+
+            RoundedParseImageView imgView = (RoundedParseImageView) v.findViewById(android.R.id.icon);
+            ParseFile imageFile = object.getParseFile("profile_picture");
             if (imageFile != null) {
                 imgView.setParseFile(imageFile);
                 imgView.loadInBackground();
@@ -169,9 +164,9 @@ public class WanderersFragment extends Fragment{
 
             TextView lbl = (TextView) v.findViewById(R.id.profile_name);
 
-            lbl.setText(c.getString("first_name") + " " + c.getString("last_name"));
+            lbl.setText(object.getString("first_name") + " " + object.getString("last_name"));
 
-            if (c.getBoolean("online")) {
+            if (object.getBoolean("online")) {
                 imgView.setBorderColor(Color.parseColor("#32CD32"));
             } else {
                 imgView.setBorderColor(Color.GRAY);
