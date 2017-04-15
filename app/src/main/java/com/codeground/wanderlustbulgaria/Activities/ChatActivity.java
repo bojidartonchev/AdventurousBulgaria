67c745db77249e35cb4a508692a626680e1af4d4
bojidar.tonchev <bojidar.tonchev@gmail.com>
2017-04-15 13:21:11 +0300

    * Bug fixed: Chat activity's style is different from the app's default one

--- app/src/main/java/com/codeground/wanderlustbulgaria/Activities/ChatActivity.java
+++ app/src/main/java/com/codeground/wanderlustbulgaria/Activities/ChatActivity.java
@@ -1,10 +1,10 @@
 package com.codeground.wanderlustbulgaria.Activities;
 
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.text.format.DateUtils;
 import android.view.MenuItem;
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -36,7 +37,7 @@ import java.util.List;
  * all the conversation messages between two users and also allows the user to
  * send and receive messages.
  */
-public class ChatActivity extends CustomActivity {
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * The Conversation list.
@@ -93,25 +94,18 @@ public class ChatActivity extends CustomActivity {
         txt.setInputType(InputType.TYPE_CLASS_TEXT
                 | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
 
-        setTouchNClick(R.id.btnSend);
+        Button sendBtn = (Button) findViewById(R.id.btnSend);
+        sendBtn.setOnClickListener(this);
 
         buddyUsername = getIntent().getStringExtra("username");
 
-        ActionBar actionBar = getActionBar();
-        if(actionBar != null){
-            actionBar.setTitle(getIntent().getStringExtra("full_name"));
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setTitle(getIntent().getStringExtra("full_name"));
+            getSupportActionBar().setDisplayUseLogoEnabled(true);
+            getSupportActionBar().setLogo(R.drawable.icon);
         }
-
-        //ParseQuery q = ParseUser.getQuery().whereEqualTo("username", buddyUsername);
-        //q.getFirstInBackground(new GetCallback<ParseUser>() {
-        //    @Override
-        //    public void done(ParseUser user, ParseException e) {
-        //        if(e==null)
-        //        {
-        //            buddy = user;
-        //        }
-        //    }
-        //});
 
         handler = new Handler();
     }
@@ -140,11 +134,9 @@ public class ChatActivity extends CustomActivity {
      */
     @Override
     public void onClick(View v) {
-        super.onClick(v);
         if (v.getId() == R.id.btnSend) {
             sendMessage();
         }
-
     }
 
     /**
