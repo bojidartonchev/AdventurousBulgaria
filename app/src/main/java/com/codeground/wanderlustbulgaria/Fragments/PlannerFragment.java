effe96608d37395c93186fa6773a0ba8b1a2c121
bojidar.tonchev <bojidar.tonchev@gmail.com>
2017-05-12 19:08:56 +0300

    + New style for each plan row in the Planner tab

--- app/src/main/java/com/codeground/wanderlustbulgaria/Fragments/PlannerFragment.java
+++ app/src/main/java/com/codeground/wanderlustbulgaria/Fragments/PlannerFragment.java
@@ -2,22 +2,28 @@ package com.codeground.wanderlustbulgaria.Fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.codeground.wanderlustbulgaria.Activities.SubmitTravellerActivity;
+import com.codeground.wanderlustbulgaria.Activities.UserHomeActivity;
 import com.codeground.wanderlustbulgaria.Interfaces.IOnItemClicked;
 import com.codeground.wanderlustbulgaria.R;
 import com.codeground.wanderlustbulgaria.Utilities.Adapters.PlannerCalendarAdapter;
@@ -25,6 +31,10 @@ import com.codeground.wanderlustbulgaria.Utilities.CenterLayoutManager;
 import com.codeground.wanderlustbulgaria.Utilities.NotificationsManager;
 import com.codeground.wanderlustbulgaria.Utilities.ParseUtils.ParseLocation;
 import com.codeground.wanderlustbulgaria.Utilities.RoundedParseImageView;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
@@ -35,7 +45,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+
+import bolts.Task;
 
 public class PlannerFragment extends Fragment implements ParseQueryAdapter.OnQueryLoadListener,View.OnClickListener, IOnItemClicked {
 
@@ -207,19 +220,87 @@ public class PlannerFragment extends Fragment implements ParseQueryAdapter.OnQue
 
             super.getItemView(object, v, parent);
 
-            RoundedParseImageView imgView = (RoundedParseImageView) v.findViewById(android.R.id.icon);
-            //ParseFile imageFile = object.getParseFile("profile_picture");
-            if (false) { //imageFile != null
-                imgView.setParseFile(null);
-                imgView.loadInBackground();
-            } else {
-                imgView.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
-            }
+            final RoundedParseImageView userProfilePicture = (RoundedParseImageView) v.findViewById(android.R.id.icon);
 
-            TextView lbl = (TextView) v.findViewById(R.id.profile_name);
+            final ParseUser origin = object.getParseUser("origin_user");
+            origin.fetchIfNeededInBackground(new GetCallback<ParseObject>() {
+                @Override
+                public void done(ParseObject object, ParseException e) {
+                    if(e==null && object != null){
+                        ParseFile imageFile = object.getParseFile("profile_picture");
+                        if (imageFile != null) {
+                            userProfilePicture.setParseFile(imageFile);
+                            userProfilePicture.loadInBackground();
+                        } else {
+                            userProfilePicture.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+                        }
+                    }else {
+                        userProfilePicture.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+                    }
+                }
+            });
 
-            lbl.setText(object.getString("origin_user_name") + " " + object.getString("to_location_name"));
+            final ParseGeoPoint fromLocation = object.getParseGeoPoint("from_location");
+            final Task<ParseLocation> targetLocationTask = object.getParseObject("to_location").fetchIfNeededInBackground();
 
+            final ImageButton dropDownButton = (ImageButton) v.findViewById(R.id.plan_drop_down_button);
+            dropDownButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Creating the instance of PopupMenu
+                    final PopupMenu popup = new PopupMenu(v.getContext(), dropDownButton);
+                    //Inflating the Popup using xml file
+                    popup.getMenuInflater()
+                            .inflate(R.menu.plan_row_menu, popup.getMenu());
+
+                    //registering popup with OnMenuItemClickListener
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()){
+                                case R.id.viewInMaps:
+                                    {
+                                        ParseLocation targetLocation = targetLocationTask.getResult();
+                                        if(targetLocation!=null){
+                                            Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                                                    Uri.parse(String.format(Locale.US, "http://maps.google.com/maps?saddr=%f,%f&daddr=%f,%f", fromLocation.getLatitude(),
+                                                                                                                                        fromLocation.getLongitude(),
+                                                                                                                                        targetLocation.getLocation().getLatitude(),
+                                                                                                                                        targetLocation.getLocation().getLongitude())));
+                                            startActivity(intent);
+                                        }
+
+                                    }
+                                    break;
+                                case R.id.viewProfileBtn:
+                                {
+                                    Intent intent = new Intent(getActivity(), UserHomeActivity.class);
+                                    intent.putExtra("userID", origin.getObjectId());
+                                    startActivity(intent);
+                                }
+                                    break;
+                                default:
+                                    popup.dismiss();
+                                    break;
+                            }
+                            return true;
+                        }
+                    });
+
+                    popup.show(); //showing popup menu
+                }
+            });
+
+            TextView startLocationLabel = (TextView) v.findViewById(R.id.start_loc_label);
+            TextView endLocationLabel = (TextView) v.findViewById(R.id.end_loc_label);
+            startLocationLabel.setText(object.getString("from_city").replaceAll(" ", "\n"));
+            endLocationLabel.setText(object.getString("to_location_name").replaceAll(" ", "\n"));
+
+            TextView userName = (TextView) v.findViewById(R.id.profile_name);
+            userName.setText(object.getString("origin_user_name"));
+
+            TextView timeLabel = (TextView) v.findViewById(R.id.time_label);
+            DateFormat df = new DateFormat();
+            timeLabel.setText(df.format("hh:mm a", object.getDate("travel_date")));
 
             return v;
         }
